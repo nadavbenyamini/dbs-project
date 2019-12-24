a8952bc7a11cc0ce4bcb8362a736cf132a36e3c6
nadavbenyamini <nadav.benyamini6@gmail.com>
2019-12-24 17:39:01 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import pymysql
+app = Flask(__name__)
+
+
+def test_query():
+    cnx = pymysql.connect(user='DbMysql03', password='prodigy', host='mysqlsrv1.cs.tau.ac.il', database='DbMysql03')
+    cursor = cnx.cursor
+    query = ("select * from artists")
+    cursor.execute(query)
+
+    res = {}
+    for (id, name) in cursor:
+        res[id] = name
+
+    cnx.close()
+    return res
+
+
+@app.route('/')
+def hello_world():
+    return test_query()
+    #  return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(port=8888, host="0.0.0.0", debug=True)
