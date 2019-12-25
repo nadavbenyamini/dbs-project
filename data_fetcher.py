f316796b090fbe602aea43c66fe147b7fb603ea9
nadavbenyamini <nadav.benyamini6@gmail.com>
2019-12-25 11:28:01 +0200

    Basic connection to musix ready

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,11 +1,7 @@
 import requests
 
 
-ROOT_URL = 'https://api.musixmatch.com/ws/1.1/'
-
-
-def fetch_data():
-    url = 'chart.artists.get?page=1&page_size=10&country=us'
-    full_url = '{}/{}'.format(ROOT_URL, url)
-    response = requests.get(full_url)
+def fetch(url, headers={}):
+    response = requests.get(url, headers=headers)
     print(response.content)
+    return response.content
