65429ec4613c69b7eb9e2d6eb1e96d97dc3316c2
nadavbenyamini <nadav.benyamini6@gmail.com>
2019-12-30 14:19:58 +0200

    Added an example for predicthq api

--- musix_match_api.py
+++ musix_match_api.py
@@ -5,6 +5,6 @@ API_KEY = 'c7692882e8a5eb99f12a16a6e176d2e2'
 headers = {'Content-Type': 'application/json', 'apikey': API_KEY}
 
 
-def fetch_musix(params):
+def fetch_musix_internal(params):
     url = '{}/{}&apikey={}'.format(ROOT_URL, params, API_KEY)
     return fetch(url)
