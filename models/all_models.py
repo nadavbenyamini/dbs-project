c5490bf39b7d02f111938b38f7b2a9b7299366ab
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-01 19:35:28 +0200

    Added country model as input of tracks_chart fetcher

--- models/all_models.py
+++ models/all_models.py
@@ -2,5 +2,6 @@
 from models.artist import Artist
 from models.track import Track
 from models.album import Album
+from models.country import Country
 from models.base_model import BaseModel
 from models.data_types import *
