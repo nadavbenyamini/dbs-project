8eca37782dfb893b6f8cc3dfa3c4b79c1ac0b5a9
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-01 18:16:21 +0200

    Passing column names to query builder

--- data_fetch/db_utils.py
+++ data_fetch/db_utils.py
@@ -1,8 +1,8 @@
 
 
 # Todo - wrap strings with '', handle Nones, add column list
-def get_insert_queries(table, records):
+def get_insert_queries(table, columns, records):
     values = ','.join(['{}'.format(tuple(rec)) for rec in records])
-    query = "insert into {} values {};".format(table, values)
+    query = "insert into {} ({}) values {};".format(table, columns,  values)
     return [query]
 
