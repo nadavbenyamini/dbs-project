c110db0c52f45f7a276c63fbd5f21f391c8301f6
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-01 19:45:28 +0200

    Connecting once to db and then bulk executing instead of connection per query

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@ import sql_executor
 
 
 class BaseModel:
+
     def get_all_values_by_field(self, field):
         query = 'select distinct (field)s from {}'.format(self.table)
         args = {'field': field}
