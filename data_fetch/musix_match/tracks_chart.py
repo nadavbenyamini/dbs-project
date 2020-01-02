2377badc24885e5bcb9d5eaf4d21d3ae2d78af6d
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 12:04:15 +0200

    First basic API to DB flow works

--- data_fetch/musix_match/tracks_chart.py
+++ data_fetch/musix_match/tracks_chart.py
@@ -9,10 +9,13 @@ class TracksChartPath(MusixFetcher):
         self.models = [Track()]
 
     def prepare_requests(self):
+        pages = [1, 2, 3]
+        page_size = 100
         countries = Country().get_all_values_by_field('country_code')
         requests = []
         for country in countries:
-            requests.append({'country': country})
+            for p in pages:
+                requests.append({'country': country, 'page': p, 'page_size': page_size})
         return requests
 
     def response_to_items(self, response):
@@ -21,6 +24,8 @@ class TracksChartPath(MusixFetcher):
         for t in track_list:
             track = t['track']
             item = {k: v for k, v in track.items() if k != 'primary_genres'}  # Cloning
+
+            item['track_release_date'] = track['updated_time']
             genre_list = track.get('primary_genres', {}).get('music_genre_list', [])
             if len(genre_list) > 0:
                 item['music_genre_id'] = genre_list[0]['music_genre']['music_genre_id']
--- database/db_utils.py
+++ database/db_utils.py
@@ -1,8 +1,9 @@
 
 
 def get_insert_queries(table, columns, records):
-    values = ','.join([tuple(rec) for rec in records])
-    query = "insert into {} {} values {};".format(table, tuple(columns),  values)
+    values = ','.join(['{}'.format(tuple(rec)) for rec in records])
+    columns_string = '(' + ', '.join(['{}'.format(c.replace("'", "")) for c in columns]) + ')'
+    query = "insert into {} {} values {};".format(table, columns_string, values)
     print(query)
     return [query]
 
