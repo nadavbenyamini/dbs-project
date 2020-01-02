d7f768bb0006fbca11256b9408aed34887805d63
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 23:57:42 +0200

    Adjusting field names

--- data_fetch/mourits/mourits_fetcher.py
+++ data_fetch/mourits/mourits_fetcher.py
@@ -11,19 +11,19 @@ class MouritsFetcher(BaseFetcher):
         self.models = [Lyrics()]
 
     def prepare_requests(self):
-        tracks = Track().get_distinct_values_by_multiple_fields('artist_id', 'track_name', 'track_id')
-        artists = Artist().get_distinct_values_by_multiple_fields('artist_id', 'artist_name')
+        tracks = Track().get_distinct_values_by_multiple_fields(['artist_id', 'track_name', 'track_id'])
+        artists = Artist().get_distinct_values_by_multiple_fields(['artist_id', 'artist_name'])
         requests = []
         for t in tracks:
             for a in artists:
                 if t[0] == a[0]:
-                    requests.append({'a': a[1], 's': t[1], 'track_id': a[2], 'separator': '\n'})
+                    requests.append({'a': a[1], 's': t[1], 'track_id': t[2], 'separator': '\n'})
         return requests
 
     def response_to_items(self, request, response):
         assert response['success']
         item = {
             'track_id': request['track_id'],
-            'lyrics': response['result']['lyrics']
+            'track_lyrics': response['result']['lyrics']
         }
         return [item]
