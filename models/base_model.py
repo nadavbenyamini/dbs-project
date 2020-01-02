fd62eeba4653e7f2333b22f26339b31cf0ed76e8
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 18:36:25 +0200

    Created a function that returns unique tuples by multiple columns

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,11 @@ from database import sql_executor
 
 class BaseModel:
 
-    def get_all_values_by_field(self, field):
+    def get_distinct_values_by_field(self, field):
+        """
+        :param field: db column
+        :return: values: list of unique values from that column
+        """
         query = 'select distinct {} from {}'.format(field, self.table)
         results = sql_executor.select(query)
         rows = results['rows']
@@ -11,4 +15,21 @@ class BaseModel:
         results = set()
         for row in rows:
             results.add(row[field_index])
+        return list(results)
+
+    def get_distinct_values_by_multiple_fields(self, fields):
+        """
+        :param fields: list of db columns
+        :return: values: list of unique tuples, each tuple consisted of the relevant columns
+        """
+        query = 'select distinct {} from {}'.format(', '.join(fields), self.table)
+        results = sql_executor.select(query)
+        rows = results['rows']
+        field_indexes = [results['headers'].index(f) for f in fields]
+        results = set()
+        for row in rows:
+            values = []
+            for ind in field_indexes:
+                values.append(row[ind])
+            results.add(tuple(values))
         return list(results)
