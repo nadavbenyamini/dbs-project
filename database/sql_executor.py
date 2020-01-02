0c0301757b2774185afa52341312c013fa559e83
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 11:34:40 +0200

    Added test route for simple select * from queries

--- database/sql_executor.py
+++ database/sql_executor.py
@@ -3,7 +3,7 @@ from sshtunnel import SSHTunnelForwarder
 import json
 
 
-def get_connection():
+def execute(queries, args={}):
 
     with open("./config/mysql_config.json") as mysql_conf_file:
         mysql_conf = json.load(mysql_conf_file)
@@ -28,25 +28,24 @@ def get_connection():
 
         ssh_conf_file.close()
         mysql_conf_file.close()
-        return pymysql.connect(host='127.0.0.1',
-                               user=sql_username,
-                               passwd=sql_password,
-                               db=sql_main_database,
-                               port=tunnel.local_bind_port)
+        db = pymysql.connect(host='127.0.0.1',
+                             user=sql_username,
+                             passwd=sql_password,
+                             db=sql_main_database,
+                             port=tunnel.local_bind_port)
+
+        cursors = []
+        for query in queries:
+            cur = db.cursor(pymysql.cursors.DictCursor)
+            cur.execute(query=query['query'], args=query['args'])
+            db.commit()
+            cursors.append(cur)
+    return cursors
 
 
-def execute(query, args={}, connection=None):
-    cur = connection.cursor(pymysql.cursors.DictCursor)
-    print(query % args)
-    cur.execute(query, args=args)
-    connection.commit()
-    return cur
-
-
-def select(query, args={}, connection=None):
-    if connection is None:
-        connection = get_connection()
-    cursor = execute(query=query, args=args, connection=connection)
+def select(query, args):
+    cursors = execute([{'query': query, 'args': args}])
+    cursor = cursors[0]
     try:
         res = {'headers': [], 'rows': []}
         for row in cursor:
@@ -56,19 +55,30 @@ def select(query, args={}, connection=None):
         return res
     finally:
         cursor.close()
-        connection.close()
 
 
-def insert(query, args={}, connection=None):
-    if connection is None:
-        connection = get_connection()
-    cursor = execute(query=query % args, connection=connection)  # TODO - change this line to prevent SQL Injection
+def insert(query, args):
+    cursors = execute([{'query': query, 'args': args}])
+    cursor = cursors[0]
     try:
-        response = cursor.fetchone()
+        response = cursor.fetchall()
         print(response)
         return response
     except Exception as e:
         raise e
     finally:
         cursor.close()
-        connection.close()
+
+
+def insert_bulk(queries):
+    final_queries = [{'query': q, 'args': {}} for q in queries]
+    cursors = execute(final_queries)
+    responses = []
+    for cur in cursors:
+        try:
+            responses.append(cur.fetchone())
+        except Exception as e:
+            responses.append(str(e))
+        finally:
+            cur.close()
+    return responses
