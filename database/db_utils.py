d9b43afffcb7feef95a24cb38757de71087487bf
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 11:49:31 +0200

    Query fix in db_utils

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,7 +1,8 @@
 
 
 def get_insert_queries(table, columns, records):
-    values = ','.join(['{}'.format(tuple(rec)) for rec in records])
+    values = ','.join([tuple(rec) for rec in records])
     query = "insert into {} {} values {};".format(table, tuple(columns),  values)
+    print(query)
     return [query]
 
