50920e2d8c88ab57b51ad0846dea8eb6e970bf2d
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 18:44:45 +0200

    Finished mourits lyrics fetcher

--- data_fetch/mourits/mourits_fetcher.py
+++ data_fetch/mourits/mourits_fetcher.py
@@ -1,4 +1,5 @@
 from data_fetch.base_fetcher import BaseFetcher
+from models.all_models import *
 
 
 class MouritsFetcher(BaseFetcher):
@@ -6,7 +7,23 @@ class MouritsFetcher(BaseFetcher):
         super().__init__()
         self.base_url = 'https://mourits.xyz:2096'
         self.headers = {'Content-Type': 'application/json'}
+        self.path = ''
+        self.models = [Lyrics()]
 
-    def response_to_items(self, response):
-        return [response['message']['body']['artist']]
-        pass
+    def prepare_requests(self):
+        tracks = Track().get_distinct_values_by_multiple_fields('artist_id', 'track_name', 'track_id')
+        artists = Artist().get_distinct_values_by_multiple_fields('artist_id', 'artist_name')
+        requests = []
+        for t in tracks:
+            for a in artists:
+                if t[0] == a[0]:
+                    requests.append({'a': a[1], 's': t[1], 'track_id': a[2], 'separator': '\n'})
+        return requests
+
+    def response_to_items(self, request, response):
+        assert response['success']
+        item = {
+            'track_id': request['track_id'],
+            'lyrics': response['result']['lyrics']
+        }
+        return [item]
