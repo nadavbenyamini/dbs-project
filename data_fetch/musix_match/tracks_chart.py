3cb7efdcb65b034497c5049348e3ed289f812c0c
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 15:18:31 +0200

    Added insertion time and fixing NULL values in insertion queries

--- data_fetch/musix_match/tracks_chart.py
+++ data_fetch/musix_match/tracks_chart.py
@@ -11,7 +11,7 @@ class TracksChartPath(MusixFetcher):
     def prepare_requests(self):
         pages = [1, 2, 3]
         page_size = 100
-        countries = Country().get_all_values_by_field('country_code')
+        countries = Country().get_all_values_by_field('country_id')
         requests = []
         for country in countries:
             for p in pages:
@@ -28,8 +28,8 @@ class TracksChartPath(MusixFetcher):
             item['track_release_date'] = track['updated_time']
             genre_list = track.get('primary_genres', {}).get('music_genre_list', [])
             if len(genre_list) > 0:
-                item['music_genre_id'] = genre_list[0]['music_genre']['music_genre_id']
+                item['genre_id'] = genre_list[0]['music_genre']['music_genre_id']
             else:
-                item['music_genre_id'] = None
+                item['genre_id'] = None
             tracks.append(item)
         return tracks
--- database/db_utils.py
+++ database/db_utils.py
@@ -1,9 +1,20 @@
+from datetime import datetime
 
 
 def get_insert_queries(table, columns, records):
-    values = ','.join(['{}'.format(tuple(rec)) for rec in records])
-    columns_string = '(' + ', '.join(['{}'.format(c.replace("'", "")) for c in columns]) + ')'
+    columns_string = prepare_columns_string(columns)
+    values = prepare_values_string(records)
     query = "insert into {} {} values {};".format(table, columns_string, values)
     print(query)
     return [query]
 
+
+def prepare_columns_string(columns):
+    columns.append('insertion_time')
+    return '(' + ', '.join(['{}'.format(c.replace("'", "")) for c in columns]) + ')'
+
+
+def prepare_values_string(records):
+    now_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    values_str = ','.join(['{}'.format(tuple(rec) + (now_str, )) for rec in records])
+    return values_str.replace("'NULL'", "NULL")
