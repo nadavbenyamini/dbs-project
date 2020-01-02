3355f8f9309334830d4902fcffa8d8f1e40fb6a6
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-02 15:30:27 +0200

    Populating Charts table from tracks_chart fetcher

--- data_fetch/musix_match/tracks_chart.py
+++ data_fetch/musix_match/tracks_chart.py
@@ -6,30 +6,39 @@ class TracksChartPath(MusixFetcher):
     def __init__(self):
         super().__init__()
         self.path = 'chart.tracks.get'
-        self.models = [Track()]
+        self.models = [Track(), Chart()]
 
     def prepare_requests(self):
-        pages = [1, 2, 3]
+        page_count = 10
         page_size = 100
         countries = Country().get_all_values_by_field('country_id')
         requests = []
         for country in countries:
-            for p in pages:
-                requests.append({'country': country, 'page': p, 'page_size': page_size})
+            for p in range(1, len(page_count) + 1):
+                requests.append({'chart_name': 'top', 'country': country, 'page': p, 'page_size': page_size})
         return requests
 
-    def response_to_items(self, response):
+    def response_to_items(self, request, response):
         track_list = response['message']['body']['track_list']
-        tracks = []
+        country_id = request['country']
+        items = []
+        i = 0
         for t in track_list:
             track = t['track']
-            item = {k: v for k, v in track.items() if k != 'primary_genres'}  # Cloning
 
+            # Extracting Track properties
+            item = {k: v for k, v in track.items() if k != 'primary_genres'}  # Cloning
             item['track_release_date'] = track['updated_time']
             genre_list = track.get('primary_genres', {}).get('music_genre_list', [])
             if len(genre_list) > 0:
                 item['genre_id'] = genre_list[0]['music_genre']['music_genre_id']
             else:
                 item['genre_id'] = None
-            tracks.append(item)
-        return tracks
+
+            # Extracting Chart properties:
+            i += 1
+            rank = (request['page'] - 1) * request['page_size'] + i
+            track.update({'country_id': country_id, 'rank': rank})
+
+            items.append(track)
+        return items
