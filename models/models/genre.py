696bfbefc4afb66ac02862a7adea54f5fbf05e35
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-03 08:56:39 +0200

    fixed genres table name

--- models/models/genre.py
+++ models/models/genre.py
@@ -4,7 +4,7 @@ from models.data_types import *
 
 class Genre(BaseModel):
     def __init__(self):
-        self.table = 'Genre'
+        self.table = 'Genres'
         self.fields = [
             {'name': 'genre_id', 'type': Types.INT, 'pk': True},
             {'name': 'genre_parent_id', 'type': Types.INT, 'pk': False},
