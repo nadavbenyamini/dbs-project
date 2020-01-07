9808997ea417231bb8b30f503e83802bfaccdba5
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-07 21:53:37 +0200

    Added population to country model, pulling charts for top countries

--- data_fetch/musix_match/tracks_chart.py
+++ data_fetch/musix_match/tracks_chart.py
@@ -11,7 +11,7 @@ class TracksChartPath(MusixFetcher):
     def prepare_requests(self):
         page_count = 10
         page_size = 100
-        countries = self.get_values_by_field(model=Country(), field='country_id')
+        countries = self.get_values_by_field(model=Country(), field='country_id', order_by='population', desc=True)
         requests = []
         for country in countries:
             for p in range(1, page_count + 1):
