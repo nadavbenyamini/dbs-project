6c7a9bb1a882d61488261762b09021cd2e9cc79b
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-13 18:01:14 +0200

    Basic country template

--- routes/app_routes.py
+++ routes/app_routes.py
@@ -5,6 +5,26 @@ from database import sql_executor
 app_routes = Blueprint('app_routes', __name__)
 
 
+@app_routes.route('/country/<country_id>')
+def country_chart(country_id):
+    query = "select t.*, c.*, ch.track_rank " \
+            "  from Countries c " \
+            "  join Charts ch " \
+            "    on c.country_id = ch.country_id" \
+            "  join Tracks t" \
+            "    on t.track_id = ch.track_id" \
+            " where c.country_id = %s" \
+            " order by track_rank"
+    args = (country_id, )  # Converting to tuple...
+    db_results = sql_executor.select(query=query, args=args)
+    tracks = []
+    headers = db_results['headers']
+    for row in db_results['rows']:
+        tracks.append({headers[i]: row[i] for i in range(len(headers))})
+    country_name = tracks[0]['country_name']
+    return render_template(template_name_or_list="country.html", tracks=tracks, country_name=country_name)
+
+
 @app_routes.route('/show/<tab_name>/<limit>')
 def show_table(tab_name, limit=100):
     query = "select * from {} limit {}".format(tab_name, limit)  # TODO - prevent SQL Injection
