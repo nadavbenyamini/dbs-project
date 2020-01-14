1dc1abbe2f9c86af5e561328cb34af789a206bd5
eyal.borovsky <eyal.borovsky@bioeyetech.com>
2020-01-14 22:29:04 +0200

    cilents_ first tables

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from data_fetch.data_fetch_routes import data_fetch_routes
 from web_app.app_routes import app_routes
 from config import *
 
+# initialization
+
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(data_fetch_routes)
 app.register_blueprint(app_routes)
 
