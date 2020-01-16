69be4b3e27bef1c271bb49cc6f0794139dde1d52
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-16 10:16:18 +0200

    Small re-org of paths

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_cors import CORS
 from data_fetch.data_fetch_routes import data_fetch_routes
 from web_app.app_routes import app_routes
+from web_app.app_logic import *
 from config import *
 
 # initialization
@@ -20,6 +21,24 @@ def home():
 @app.route('/artist/<artist_id>')
 def artist(artist_id):
     return render_template("artist.html", artist_id=artist_id)
+
+
+@app.route('/artist/<track_id>')
+def track(track_id):
+    render_template("track.html", track_id=track_id)
+
+
+# --------------- TODO - Delete the following routes ---------------- #
+@app_routes.route('/show/<tab_name>/<limit>')
+def show_table(tab_name, limit=100):
+    rows = get_all_from_table(tab_name, limit)
+    return render_template(template_name_or_list="old/base.html", rows=rows, title=tab_name)
+
+
+@app_routes.route('/test')
+def test():
+    return render_template(template_name_or_list="old/charts_albums.html", base_url=BASE_URL)
+# -------------------------------------------------------------------- #
 
 
 if __name__ == '__main__':
