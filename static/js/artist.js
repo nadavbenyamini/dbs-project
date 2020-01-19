7f8b132c5763ab8d4b7de4a2361a98585d1a7c11
Dana <daniela.fishman20@gmail.com>
2020-01-19 22:07:53 +0200

    working

--- static/js/artist.js
+++ static/js/artist.js
@@ -22,10 +22,9 @@ function getArtistInfoTable() {
             {title:"artist_country_id", field:"artist_country_id", visible:false},
             {title:"Country Name", field:"artist_country_name"},
             {title:"Id", field:"artist_id", visible:false},
-            {title:"Rating", field:"artist_rating", align:"left", formatter:"star"},
-            {title:"total_tracks_in_charts", field:"total_tracks_in_charts"},
-            {title:"Genere", field:"genre_name"},
-            {title:"unique_country_charts", field:"unique_country_charts"}
+            {title:"Rating", field:"artist_rating", align:"left"},
+            {title:"Total tracks in charts", field:"total_tracks_in_charts"},
+            {title:"Number of countries in charts", field:"unique_country_charts"}
         ]
     });
 }
@@ -47,8 +46,7 @@ function getTracksTable() {
             {title:"Id", field:"al.artist_id", visible:false},
             {title:"Id", field:"track_id", visible:false},
             {title:"Id", field:"album_id", visible:false},
-            {title:"Name", field:"track_name", width:150},
-            {title:"Rating", field:"track_rating", align:"left", formatter:"star"},
+            {title:"Name", field:"track_name"},
             {title:"Album", field:"album_name"},
             {title:"Genere", field:"genre_name"},
             {title:"Release Date", field:"track_release_date"}
@@ -67,12 +65,12 @@ function getChartsTable() {
             return response; //return the tableData property of a response json object
         },
         columns: [ //Define Table Columns
-            {title: "album_name", field: "album_name"},
-            {title: "country_id", field: "country_id"},
-            {title: "country_name", field: "country_name"},
-            {title: "track_id", field: "track_id", width: 150},
-            {title: "track_name", field: "track_name", align: "left"},
-            {title: "track_rank", field: "track_rank"},
+            {title: "Album Name", field: "album_name"},
+            {title: "country_id", field: "country_id", visible:false},
+            {title: "Country Name", field: "country_name"},
+            {title: "track_id", field: "track_id",visible:false},
+            {title: "Track Name", field: "track_name", align: "left"},
+            {title: "Track rank", field: "track_rank"},
         ]
     });
 }
@@ -87,7 +85,7 @@ function getSimilarTable() {
             return response; //return the tableData property of a response json object
         },
         columns: [ //Define Table Columns
-            {title: "artist_id", field: "artist_id"},
+            {title: "artist_id", field: "artist_id",visible:false},
             {title: "artist_name", field: "artist_name"},
         ]
     });
