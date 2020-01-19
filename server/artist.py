3e122e720bb329af1463a873853d5c883e1603ed
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-19 15:02:56 +0200

    Mock data with responses works

--- server/artist.py
+++ server/artist.py
@@ -7,7 +7,7 @@ artist_routes = Blueprint('artist_routes', __name__)
 
 @artist_routes.route('/api/artist/<artist_id>', methods=['GET'])
 def get_artist(artist_id):
-    return [
+    return jsonify([
   {
     "artist_country_id": "US",
     "artist_country_name": "United States",
@@ -17,12 +17,12 @@ def get_artist(artist_id):
     "total_tracks_in_charts": 6,
     "unique_country_charts": 3
   }
-]
+])
 
 
 @artist_routes.route('/api/artist_tracks/<artist_id>', methods=['GET'])
 def get_tracks_by_artist(artist_id):
-    return [
+    return jsonify([
   {
     "album_id": 10872038,
     "album_name": "Only By the Night",
@@ -47,12 +47,12 @@ def get_tracks_by_artist(artist_id):
     "track_name": "Sex on Fire",
     "track_release_date": "Fri, 22 Nov 2019 20:34:22 GMT"
   }
-]
+])
 
 
 @artist_routes.route('/api/artist_charts/<artist_id>', methods=['GET'])
 def get_charts_by_artist(artist_id):
-    return [
+    return jsonify([
   {
     "album_name": "Only By the Night",
     "country_id": "DK",
@@ -101,12 +101,169 @@ def get_charts_by_artist(artist_id):
     "track_name": "Sex on Fire",
     "track_rank": 75
   }
-]
+])
 
 
 @artist_routes.route('/api/similar_artists/<artist_id>', methods=['GET'])
 def get_similar_artists(artist_id):
-    return []
+    return jsonify([
+  {
+    "artist_id": 7521,
+    "artist_name": "Red Hot Chili Peppers"
+  },
+  {
+    "artist_id": 1039,
+    "artist_name": "Coldplay"
+  },
+  {
+    "artist_id": 44890,
+    "artist_name": "Kings of Leon"
+  },
+  {
+    "artist_id": 512390,
+    "artist_name": "The Rumour Said Fire"
+  },
+  {
+    "artist_id": 70787,
+    "artist_name": "The Killers"
+  },
+  {
+    "artist_id": 106606,
+    "artist_name": "Feist"
+  },
+  {
+    "artist_id": 340352,
+    "artist_name": "Lykke Li"
+  },
+  {
+    "artist_id": 96,
+    "artist_name": "The Cranberries"
+  },
+  {
+    "artist_id": 416,
+    "artist_name": "The Cure"
+  },
+  {
+    "artist_id": 13895270,
+    "artist_name": "Imagine Dragons"
+  },
+  {
+    "artist_id": 210475,
+    "artist_name": "Up Dharma Down"
+  },
+  {
+    "artist_id": 349065,
+    "artist_name": "Jamie Woon"
+  },
+  {
+    "artist_id": 32364,
+    "artist_name": "M83"
+  },
+  {
+    "artist_id": 10528,
+    "artist_name": "Gossip"
+  },
+  {
+    "artist_id": 14344136,
+    "artist_name": "American Authors"
+  },
+  {
+    "artist_id": 13899662,
+    "artist_name": "Willow"
+  },
+  {
+    "artist_id": 8891,
+    "artist_name": "Gorillaz"
+  },
+  {
+    "artist_id": 27804801,
+    "artist_name": "KALEO"
+  },
+  {
+    "artist_id": 26475844,
+    "artist_name": "Kwabs"
+  },
+  {
+    "artist_id": 25781276,
+    "artist_name": "Rag'n'Bone Man"
+  },
+  {
+    "artist_id": 24444227,
+    "artist_name": "Glass Animals"
+  },
+  {
+    "artist_id": 14358589,
+    "artist_name": "Cigarettes After Sex"
+  },
+  {
+    "artist_id": 33932733,
+    "artist_name": "King Gnu"
+  },
+  {
+    "artist_id": 29247465,
+    "artist_name": "Billie Eilish"
+  },
+  {
+    "artist_id": 27926347,
+    "artist_name": "Gerry Cinnamon"
+  },
+  {
+    "artist_id": 35293137,
+    "artist_name": "The FAIM"
+  },
+  {
+    "artist_id": 13805436,
+    "artist_name": "Lana Del Rey"
+  },
+  {
+    "artist_id": 33091369,
+    "artist_name": "Shatta Wale"
+  },
+  {
+    "artist_id": 24676820,
+    "artist_name": "SHAED"
+  },
+  {
+    "artist_id": 93674,
+    "artist_name": "LP"
+  },
+  {
+    "artist_id": 31861215,
+    "artist_name": "Dominic Fike"
+  },
+  {
+    "artist_id": 37052122,
+    "artist_name": "Hindia"
+  },
+  {
+    "artist_id": 36235669,
+    "artist_name": "Lil Nas X"
+  },
+  {
+    "artist_id": 38851741,
+    "artist_name": "BENEE"
+  },
+  {
+    "artist_id": 37843472,
+    "artist_name": "Tones and I"
+  },
+  {
+    "artist_id": 36940461,
+    "artist_name": "Tems"
+  },
+  {
+    "artist_id": 24396907,
+    "artist_name": "Milky Chance"
+  },
+  {
+    "artist_id": 40355032,
+    "artist_name": "Elaine"
+  },
+  {
+    "artist_id": 29695422,
+    "artist_name": "Kofi Kinaata"
+  }
+])
 
 
 @artist_routes.route('/api/artists', methods=['GET'])
