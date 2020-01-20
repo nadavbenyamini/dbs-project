189fb0a4175db28561c243315dba16c67a51d233
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-20 17:18:50 +0200

    Updated config files to look more like server

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from sshtunnel import SSHTunnelForwarder
-HOST = "127.0.0.1"
-PORT = 5001
+HOST = "0.0.0.0"
+PORT = 40444
 BASE_URL = HOST + ":" + str(PORT)
 USE_SSH = True
--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
-const server = "127.0.0.1"
-const port = "5001"
-const port_api = port+"/api"
+const server = "127.0.0.1";
+const port = "40444";
+const port_api = port+"/api";
