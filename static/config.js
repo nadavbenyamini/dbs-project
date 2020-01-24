1731f0737f75fe2716c70835700cd8a21c4d96cf
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-01-24 15:03:02 +0200

    Added documentation to config files

--- static/config.js
+++ static/config.js
@@ -1,3 +1,3 @@
-const server = "delta-tomcat-vm.cs.tau.ac.il";
+const server = "delta-tomcat-vm.cs.tau.ac.il"; // Change to "0.0.0.0" to work locally
 const port = "40444";
 const port_api = port+"/api";
